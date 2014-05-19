c64d5179138473a090f96dc09d58236e1e8061c4
bvuillemin <benoit.vuil@gmail.com>
2014-05-19 08:40:45 +0200

    Initialisation du projet

---
+++ TP-HTTP/src/tp/http/TPHTTP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tp.http;
+
+/**
+ *
+ * @author bvuillemin <benoit.vuil@gmail.com>
+ */
+public class TPHTTP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
