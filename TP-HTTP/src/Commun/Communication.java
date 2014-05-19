4659d0279f50c227b6fd3c40f81f4fc9f938412e
PReynaud <pierre.reynaud@hotmail.fr>
2014-05-19 09:57:03 +0200

    Mise en place de la communication (suite)

--- TP-HTTP/src/Commun/Communication.java
+++ TP-HTTP/src/Commun/Communication.java
@@ -1,6 +1,11 @@
 package Commun;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Communication {
+    public InputStream in;
+    public OutputStream out;
     
     public Communication(){
     
--- TP-HTTP/src/Serveur/CommunicationServeur.java
+++ TP-HTTP/src/Serveur/CommunicationServeur.java
@@ -1,7 +1,15 @@
 package Serveur;
 
 import Commun.Communication;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class CommunicationServeur extends Communication{
     
+    public void comServeur(Socket connexion) throws IOException{
+        this.in = connexion.getInputStream();
+        
+    }
 }
