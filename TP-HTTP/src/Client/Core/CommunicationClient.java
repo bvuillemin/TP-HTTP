1f2cf8845ffd13efd819243f89bcd67a0f95b5f3
PReynaud <pierre.reynaud@hotmail.fr>
2014-06-01 22:30:01 +0200

    Correction de bugs

--- TP-HTTP/src/Client/Core/CommunicationClient.java
+++ TP-HTTP/src/Client/Core/CommunicationClient.java
@@ -4,33 +4,52 @@ import HTTP.Communication;
 import HTTP.Erreur;
 import HTTP.Reponse;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetAddress;
 
-public class CommunicationClient extends Communication{
-    
+public class CommunicationClient extends Communication {
+
     public CommunicationClient(InetAddress _ip, int _port) throws ErreurClient, Erreur {
         super(_ip, _port);
     }
-    
-    public File attente_fichier(String nom_fichier) throws ErreurClient{
+
+    public void attente_fichier(String nom_fichier) throws ErreurClient {
         try {
             byte[] b = new byte[2048];
-            Reponse rep=new Reponse();
+            Reponse rep = new Reponse();
             BufferedInputStream bi = new BufferedInputStream(this.in);
-            
+
             int read = bi.read(b);
             if (read > 0) {
                 System.out.println("Client: message reçu");
                 String res = new String(b, "UTF-8");
-                if(rep.getReponse(res)){
+                if (rep.getReponse(res)) {
                     String header = rep.get_header();
                     System.out.println(header);
-                    
+
+                    /*On récupère le répertoire courant*/
+                    String repertoire = System.getProperty("user.dir");
+                    String nomFichierFinal;
+                    //String repertoire = this.PATH;
+                    /*On vérifie si l'on doit sélectionner l'index ou non*/
+                    if (nom_fichier.equals(new String("\\"))) {
+                        nomFichierFinal = repertoire + "\\Client\\index.html";
+                    } else {
+                        nomFichierFinal = repertoire + "\\Client\\" + nom_fichier;
+                    }
+
                     /*On enregistre la data dans un fichier*/
-                    return new File(rep.getContent());
-                    
+                    FileWriter writer = new FileWriter(nomFichierFinal, false);
+                    writer.write(rep.getMessage());
+
+                    if (writer != null) {
+                        writer.close();
+                    }
+
                 }
             }
             if (read == - 1) {
@@ -39,6 +58,5 @@ public class CommunicationClient extends Communication{
         } catch (IOException ex) {
             throw new ErreurClient("Erreur dans la lecture du flux " + ex.getMessage());
         }
-        return null;
     }
 }
